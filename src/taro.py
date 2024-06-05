55db7de40d237733fac54581ed232370300ca15a
Tatsu-Turtle <138899931+Tatsu-Turtle@users.noreply.github.com>
2024-06-04 21:49:35 -0600

    Added some links to txt file.

--- src/taro.py
+++ src/taro.py
@@ -13,7 +13,7 @@ activities = ["Math", "Physics", "Gym", "Bike"]
 
 for i in range(2):
     for time in week[i].get_timeslots():
-        if 0 <= time < 2100 or 2300 < time:
+        if 0 <= time < 2200 or 2300 < time:
             week[i].book_timeslot("Sleep", time)
 
 for i in range(2):
@@ -21,8 +21,8 @@ for i in range(2):
         if week[i].get_activity(time) == "Empty":
             _ = dqm.add_variable(len(activities), label=f"{i}_{time}")
 
-dqm.set_quadratic("0_2120", "0_2200", {(0, 2): -1.5}) # Corresponds to preference in doing Math at 21:30 and then Gym at 22:00
-print(dqm.get_quadratic("0_2120", "0_2200", array=True))
+dqm.set_quadratic("0_2240", "0_2300", {(0, 2): -1.5}) # Corresponds to preference in doing Math at 21:30 and then Gym at 22:00
+print(dqm.get_quadratic("0_2240", "0_2300", array=True))
 
 
 print(dqm.variables)
@@ -30,5 +30,5 @@ print(week)
 
 
 #WARNING: Do not run on CPU when there are more than 10 variables. With i variables the possibilities are summmation e^i. source: Trust me
-# sampleset = sampler.sample_dqm(sampler, dqm)
-# print(sampleset)
+sampleset = sampler.sample_dqm(sampler, dqm)
+print(sampleset)
