02af2f0552203f0053a7fea1f78b5f5069ee8424
TDC28 <a06x03g07t01@protonmail.com>
2024-08-12 17:12:00 -0300

    Better navbar and change location of frontend

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,20 +1,24 @@
 import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/navbar";
 import { Link } from "@nextui-org/link";
+import { useLocation } from "react-router-dom"
 
 export default function Header() {
+   const location = useLocation()
+   const isActive = (path: string): boolean => location.pathname === path;
+
   return (
     <Navbar>
       <NavbarContent>
-        <NavbarItem>
+        <NavbarItem isActive={isActive("/")}>
           <Link color="foreground" href="/">Home</Link>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem isActive={isActive("/setup")}>
           <Link color="foreground" href="/setup">Setup</Link>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem isActive={isActive("/generate")}>
           <Link color="foreground" href="/generate">Generate Schedule</Link>
         </NavbarItem>
-        <NavbarItem>
+        <NavbarItem isActive={isActive("/contact")}>
           <Link color="foreground" href="/contact">Contact Us</Link>
         </NavbarItem>
       </NavbarContent>
