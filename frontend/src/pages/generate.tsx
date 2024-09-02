95b94d01e3570cab39fa6db6ffa85eb1998ccc68
TDC28 <a06x03g07t01@protonmail.com>
2024-09-02 16:01:33 -0400

    Small stuff

--- frontend/src/pages/generate.tsx
+++ frontend/src/pages/generate.tsx
@@ -1,14 +1,13 @@
-import DefaultLayout from "@/layouts/default"
-import ScheduleTable from "@/components/scheduleTable"
-import React from "react"
-import { Spacer } from "@nextui-org/react"
+import DefaultLayout from "@/layouts/default";
+import ScheduleTable from "@/components/scheduleTable";
+import { Spacer } from "@nextui-org/react";
 
 export default function GeneratePage() {
-   return (
-      <DefaultLayout>
-         <h1>This is the calendar generation page</h1>
-         <Spacer y={5} />
-         <ScheduleTable />
-      </DefaultLayout>
-   )
+  return (
+    <DefaultLayout>
+      <h1>This is the calendar generation page</h1>
+      <Spacer y={5} />
+      <ScheduleTable />
+    </DefaultLayout>
+  );
 }
