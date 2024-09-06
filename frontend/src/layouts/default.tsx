1c37dabc2de67d6875cbb4e0ae24fbc328f88838
TDC28 <a06x03g07t01@protonmail.com>
2024-09-05 20:34:44 -0400

    Home page has a better ui

--- frontend/src/layouts/default.tsx
+++ frontend/src/layouts/default.tsx
@@ -12,6 +12,15 @@ export default function DefaultLayout({
         {children}
       </main>
       <footer className="w-full flex items-center justify-center py-3">
+        <div className="flex flex-row space-x-6 justify-start">
+          <p className="mt-4">Made possible with</p>
+          <a href="https://www.qai.ca">
+            <img src="/qai.png" width="65" />
+          </a>
+          <a href="https://www.dwavesys.ca">
+            <img src="/dwave.png" width="175" height="14" />
+          </a>
+        </div>
       </footer>
     </div>
   );
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,35 @@
-import DefaultLayout from "@/layouts/default"
+import { useState, useEffect } from "react";
+import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
-   return (
-      <DefaultLayout>
-         <h1>This is the home page</h1>
-      </DefaultLayout>
-   )
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+
+  return (
+    <DefaultLayout>
+      <div>
+        <h1 className="text-5xl pb-4 text-gray-700">
+          Where quantum computing meets utility.
+        </h1>
+        <div className="flex flex-col space-y-6">
+          <div>
+            <p className={`text-xl font-thin`}>
+              Qalendar is an innovative calendar application powered by quantum
+              computation.
+            </p>
+            <p className="text-xl font-thin">
+              It is designed to help you optimize your schedule efficiently and
+              effectively.
+            </p>
+          </div>
+          <div>
+            <p>Home page continues...</p>
+          </div>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
 }
