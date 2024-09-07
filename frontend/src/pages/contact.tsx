ec6374ae482585ab9acbdc4deee17accdd7ec91e
TDC28 <a06x03g07t01@protonmail.com>
2024-09-06 20:17:14 -0400

    Keeping track of user input for contact us page

--- frontend/src/pages/contact.tsx
+++ frontend/src/pages/contact.tsx
@@ -8,8 +8,25 @@ import {
   CardHeader,
   Button,
 } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function ContactPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleBodyChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBody(e.target.value);
+  };
+
   return (
     <DefaultLayout>
       <div>
@@ -17,25 +34,40 @@ export default function ContactPage() {
         <div className="flex items-start gap-5">
           <Card className="w-5/12">
             <CardHeader className="pb-0 pt-2 px-4">
-              <p className="text-lg font">Send us a message</p>
+              <p className="text-lg font">Send us some feedback</p>
             </CardHeader>
             <CardBody className="flex flex-col gap-4">
-              <Input label="Your name" />
-              <Input type="email" label="Your email" />
-              <Textarea label="Message body" />
+              <Input
+                type="text"
+                value={name}
+                label="Your name"
+                onChange={handleNameChange}
+              />
+              <Input
+                type="email"
+                value={email}
+                label="Your email"
+                onChange={handleEmailChange}
+              />
+              <Textarea
+                type="text"
+                value={body}
+                label="Message body"
+                onChange={handleBodyChange}
+              />
               <Button color="primary">Submit</Button>
             </CardBody>
           </Card>
           <Card className="w-1/3">
             <CardHeader className="pb-0 pt-2 px-4">
-              <p className="text-lg font">Reach out to the developpers</p>
+              <p className="text-lg font">Connect with us</p>
             </CardHeader>
             <CardBody className="flex flex-col gap-4">
               <Card>
                 <CardBody>
                   <div className="flex flex-row items-center justify-between">
                     <div className="flex flex-row justify-start items-center gap-3">
-                      <Avatar />
+                      <Avatar src="/alex.png" />
                       <p className="pr-3">Alexandre Boutot </p>
                     </div>
                     <div className="flex flex-row gap-3 pr-2">
